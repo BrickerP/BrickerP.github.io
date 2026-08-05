561f051ca3a3371e55e794334730823835c232c0
brickerP <brickerP@users.noreply.github.com>
2026-08-06 03:00:13 +0800

    chore: remove one seam lamp for bundle safety

--- src/rendering/BeijingDriveScene.ts
+++ src/rendering/BeijingDriveScene.ts
@@ -2351,7 +2351,6 @@ export class BeijingDriveScene {
 
     this.addLamp(0.925, -5.8, false);
     this.addLamp(0.958, 5.8, false);
-    this.addLamp(0.985, -5.8, false);
   }
 
   /** Sparse, human-scale props that give the drive a believable metre scale. */
